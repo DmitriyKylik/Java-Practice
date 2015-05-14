2b723ed2ccd00d7ccd9b704c40aa252f058368d9
kylik400 <kylik400@mail.ru>
2015-05-14 21:52:51 +0400

    Practice#1

---
+++ Practice #1/src/Main.java
@@ -0,0 +1,41 @@
+
+
+/**
+ * Created by Dimon on 14.05.2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // First Task
+
+        int q = 18;
+        int w = 9;
+
+        System.out.println("Результат деления: " + (q/w) + " Остаток: " + (q%w));
+
+        //Second Task
+
+        int e = 54;
+
+
+        System.out.println((e/10+e%10));
+
+        //Third Task
+
+        double t= 24.6;
+        int y = (int)Math.round(t);
+
+
+
+        System.out.println(y);
+
+        //Fourth Task
+
+        int u = 849;
+        int i = (u/100+((u/10)%10)+u/10);
+
+        System.out.println(i);
+    }
+
+}
